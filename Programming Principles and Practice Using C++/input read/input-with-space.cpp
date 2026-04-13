f8a2b96ebd8ecb5bc6c46f6c5f352af5f5981d4d
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-04-13 04:39:37 +0000

    input-with-space.cpp,

--- Programming Principles and Practice Using C++/input read/input-with-space.cpp
+++ Programming Principles and Practice Using C++/input read/input-with-space.cpp
@@ -12,7 +12,7 @@ int main()
     std::cin.ignore(1000, '\n'); // best practice
 
     std::cout << "Enter you Name: ";
-    std::getline(std::cin, name);
+    std::getline(std::cin, name); // without operator >> so read getline function way in use cin
 
     std::cout << "Age: " << age << "\n";
     std::cout << "Name: " << name << "\n";
