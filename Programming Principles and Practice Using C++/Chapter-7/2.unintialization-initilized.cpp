772fabbf0bd252dbf581e4d51c0d6a919783bd41
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-12 06:30:59 +0000

    1.declaration-definition.cpp,2.unintialization-initilized.cpp,a.out,

---
+++ Programming Principles and Practice Using C++/Chapter-7/2.unintialization-initilized.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+
+    // =========================================
+   //  Uninitialized variable (BAD)
+   // ==========================================
+
+   /*int garbage_value;
+
+   std::cout << "Uninitialized value (dangerous): ";
+             << garbage_value
+             << '\n';*/
+
+    // ===============================================
+    // Initialized variable (Good)
+    // ============================================
+    
+    int age = 21;
+
+    double price = 199.99;
+
+    std::string name {"Vasanth"};
+
+    // ============================================
+    // Constant declarations
+    // =============================================
+
+    const double pi = 3.1415926535;
+
+    const int max_users {100};
+
+    // ==========================================
+    // Vector initilization
+    // ========================================
+
+    std::vector<int> numbers(5);
+
+    std::vector<int> scores {95, 88, 76, 100};
+
+    // =========================================
+    // Output
+    // ==========================================
+
+    std::cout << "\nInitialized variable:\n";
+
+    std::cout << "Age       :"
+              << age
+              << '\n';
+
+     std::cout << "Price    :"
+               << price
+               << '\n';
+               
+    std::cout << "Name      :"
+              << name 
+              << '\n';
+
+    std::cout << "\nConstants:\n";
+    
+    std::cout << "Pi        :"
+              << pi 
+              << '\n';
+
+    std::cout << "Max users  :  "
+              << max_users
+              << '\n';
+              
+              
+    std::cout << "\nVector with size constructor:\n";
+    
+    for (int value : numbers) {
+
+        std::cout << value << ' ';
+    }
+
+    std::cout << "\n\nVector with initializer list:\n";
+
+    for (int value : scores) {
+
+        std::cout << value << ' ';
+    }
+
+    std::cout << '\n';
+}
