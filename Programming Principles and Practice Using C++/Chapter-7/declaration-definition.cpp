b5e393534a6fb1e744f543391502493acfe433c0
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-12 05:49:36 +0000

    a.out,declaration-definition.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-7/declaration-definition.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+// =================================
+/// Declarations
+// =================================
+
+// Function declarations
+int add(int a, int b);    // declaration
+void print_result(const std::string& label, int value); //declaration
+
+// global variable declaration (extern means declaration only)
+extern int global_value; // declaration only
+
+
+// =========================================
+// Definitions
+// =========================================
+
+// global variable definition
+int global_value = 100;
+
+// function definition
+int add(int a, int b) {
+
+    return a + b;
+}
+
+// function definition
+void print_result(const std::string& label, int value) {
+
+    std::cout << label << " : " << value << '\n';
+}
+
+// ===========================================
+// Main
+// ============================================
+
+int main() {
+
+    // variable definition
+    int x = 10;
+
+    // variable definition
+    int y = 20;
+
+    // function call
+    int result = add(x, y);
+
+    // function call
+    print_result("sum", result);
+
+    // using global variable
+    std::cout << "Global value: "
+              << global_value
+              << '\n';
+}
+
