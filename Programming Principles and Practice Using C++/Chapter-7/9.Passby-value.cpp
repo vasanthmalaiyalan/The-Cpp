6dae8b1da4e1577c3731988deb7ccff421a13bce
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-18 12:42:59 +0000

    9.Passby-value.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-7/9.Passby-value.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+// ==========================================
+// Pass-by-value function
+// ==========================================
+
+int increase(int value) {
+    std::cout << "\nInside function BEFORE change:\n";
+    std::cout << "value  = " << value << '\n';
+
+    value = value + 1;
+
+    std::cout << "Inside function AFTER change:\n";
+    std::cout << "value = " << value << '\n';
+
+    return value;
+}
+
+// ==============================================
+// Another example
+// ==============================================
+
+void multiply_by_two(int number) {
+
+    number = number * 2;
+
+    std::cout << "\nInside multiply _by_two():\n";
+
+    std::cout << "number = " << number << '\n';
+}
+
+// =================================================
+// Main
+// ================================================
+
+int main() {
+    int x = 10;
+
+    std::cout << "Before function call:\n";
+    std::cout << "x = " << x << '\n';
+
+    int result = increase(x);
+
+    std::cout << "\nAfter function call:\n";
+
+    std::cout << "x = " << x << '\n';
+    std::cout << "returned result = " << result << '\n';
+
+    multiply_by_two(x);
+
+    std::cout << "\nAfter multiply_by_two():\n";
+    std::cout << "x = " << x << '\n';
+}
