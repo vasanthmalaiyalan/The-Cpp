d27756867d1cfcba2de8f48a5de1550f98c02e2a
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-19 19:03:54 +0000

    24.Header.cpp,24.Main.cpp,24.f.cpp,24.f.h,24.header.md,

---
+++ Programming Principles and Practice Using C++/Chapter-7/24.Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <f.h>
+
+int main() {
+
+    int result = square(5);
+
+    std::cout << "result = " << result << '\n';
+}
---
+++ Programming Principles and Practice Using C++/Chapter-7/24.f.h
@@ -0,0 +1,10 @@
+#ifndef F_H
+#define F_H
+
+// =====================================
+// Function declaration
+// ====================================
+
+int square(int x);
+
+#endif
