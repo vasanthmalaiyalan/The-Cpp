c0a54b72ab995c400c0ea49582dc60c57dc4f534
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-21 09:10:53 +0000

    Classes-and-Members.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-8/Classes-and-Members.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+class BankAccount {
+    public:
+      std::string owner; // data member
+      int balance; // data member
+
+      // member function
+      void deposit(int money) {
+        balance += money;
+      }
+
+      // memeber function
+      void withdraw(int money) {
+        balance  -= money;
+      }
+
+      // member function
+      void show() {
+        std::cout << "Owner   : " << owner << '\n';
+        std::cout << "Balance : " << balance << '\n';
+      }
+};
+
+int main() {
+
+    // object creation
+    BankAccount acc;
+
+    // accessing data members
+    acc.owner = "Vasanth";
+    acc.balance = 1000;
+
+    // calling member functions
+    acc.deposit(500);
+    acc.withdraw(200);
+
+    // display final state
+    acc.show();
+}
