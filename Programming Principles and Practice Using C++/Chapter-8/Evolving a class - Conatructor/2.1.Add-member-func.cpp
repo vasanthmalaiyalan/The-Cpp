83a449007e3e9e65793d97e8bc5922aafbf1b4eb
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-23 17:13:16 +0000

    2.1.Add-member-func.cpp,a.out,

---
+++ Programming Principles and Practice Using C++/Chapter-8/Evolving a class - Conatructor/2.1.Add-member-func.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Date {
+    private:
+      int day;
+      int month;
+      int year;
+
+    public:
+       Date(int d, int m, int y) : day(d), month(m), year(y) {}
+       
+       int get_day() const {
+        return day;
+       }
+
+       int get_month() const {
+        return month;
+       }
+
+       int get_year() const {
+        return year;
+       }
+
+       void print() const {
+
+        std::cout << day << '/' << month << '/' << year << '\n';
+       }
+
+       void add_day(int m) {
+
+          day += n;
+       }
+};
+
+int main() {
+
+    Date today(23, 5, 2026);
+
+    today.print();
+
+    today.add_day(5);
+
+    today.print();
+}
