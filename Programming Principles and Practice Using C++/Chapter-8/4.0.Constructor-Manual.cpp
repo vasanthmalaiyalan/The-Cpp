e3c636216a207a89a331ec29b8b826502b556062
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-25 08:14:38 +0000

    4.0.Constructor-Manual.cpp,a.out,

---
+++ Programming Principles and Practice Using C++/Chapter-8/4.0.Constructor-Manual.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdexcept>
+
+struct Date {
+
+    int y;
+    int m;
+    int d;
+
+    Date(int year, int month, int day) {
+
+        if (month < 1 || month > 12) {
+            throw std::runtime_error("invalid month");
+        }
+
+        if (day < 1 || day > 31) {
+            throw std::runtime_error("invalid day");
+        }
+
+        y = year;
+        m = month;
+        d = day;
+    }
+
+    void add_day(int n) {
+        d += n;
+    }
+};
+
+int main() {
+
+    Date today{2025,12,13};
+
+    today.add_day(1);
+
+    std::cout << today.d << '/' << today.m << '/' << today.y << '\n';
+}
