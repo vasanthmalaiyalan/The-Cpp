dbdcc049758525482de87d9852d6783ab0b1110d
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-27 06:08:32 +0000

    8.3.Perfect-demo.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-8/Enum Operator Overloading/8.3.Perfect-demo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+enum class Month {
+
+    jan = 1,
+    feb = 2,
+    mar = 3
+};
+
+struct Point {
+    int x;
+    int y;
+};
+
+int main() {
+
+    Month m = Month::feb;
+
+    std::cout << "Month value = " << static_cast<int>(m) << '\n';
+    std::cout << "Address of object m = " << &m << '\n';
+
+    Month m2{2};
+
+    if (m2 == Month::feb) {
+        std::cout << "Month{2} == Month::feb\n";
+    }
+
+    Point p{10, 20};
+
+    std::cout << "Point = " << p.x << " " << p.y << '\n';
+}
