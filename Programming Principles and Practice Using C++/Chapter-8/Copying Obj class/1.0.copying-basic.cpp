f10c8fdc0e6cd09dff59792e4571d8aaa8338782
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-27 09:45:07 +0000

    1.0.copying-basic.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-8/Copying Obj class/1.0.copying-basic.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+enum class Month {
+
+    jan = 1,
+    feb,
+    mar,
+    apr,
+    may,
+    jun,
+    jul,
+    aug,
+    sep,
+    oct,
+    nov,
+    dec
+};
+
+struct Year {
+    int y;
+};
+
+class Date {
+    public:
+      Date(Year yy, Month mm, int dd) : y{yy}, m{mm}, d{dd} {}
+
+      void print() const {
+        std::cout << y.y << '/' << static_cast<int>(m) << '/' << d << '\n';
+      }
+
+    private:
+      year y;
+      Month m;
+      int d;  
+};
+
+int main() {
+
+    Date holiday {
+        Year{1978},
+        Month::jul,
+        4
+    };
+
+    Date d2 = holiday;
+
+    d2.print();
+}
