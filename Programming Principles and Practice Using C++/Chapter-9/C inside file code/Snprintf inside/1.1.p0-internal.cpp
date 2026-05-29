a6c7750dfb5ad9ecaeb4372ee80b1ea666d5ca58
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-29 16:29:58 +0000

    1.1.p0-internal.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-9/C inside file code/Snprintf inside/1.1.p0-internal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+    char text[] = "ABC";
+
+    char* p = text;
+
+    std::cout << "=== for loop: p fixed, i change ====\n";
+
+    for (int i = 0; i < 3; i++) {
+
+        std::cout << "p address : " << (void*)p << '\n';
+        std::cout << "i         : " << i << '\n';
+        std::cout << "p[i]      : " << p[i] << '\n\n';
+    }
+
+    std::cout << "=== p++: i fixed, p changes ===\n";
+
+    p = text;
+
+    for (int i = 0; i < 3; i++) {
+
+        std::cout << "p address : " << (void*)p << '\n';
+        std::cout << "i         : " << 0 << '\n';
+        std::cout << "p[0]      : " << p[0] << '\n\n';
+        p++;
+    }
+
+    return 0;
+}
