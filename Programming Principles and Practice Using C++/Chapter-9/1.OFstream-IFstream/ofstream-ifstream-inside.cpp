6207a07869f59740c007e71069d641e7726ac3d2
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-29 17:46:28 +0000

    ofstream-ifstream-inside.cpp,

--- Programming Principles and Practice Using C++/Chapter-9/1.OFstream-IFstream/ofstream-ifstream-inside.cpp
+++ Programming Principles and Practice Using C++/Chapter-9/1.OFstream-IFstream/ofstream-ifstream-inside.cpp
@@ -35,5 +35,82 @@ class MyOfstream {
            char buffer[100];
 
            snprintf(buffer, sizeof(buffer), "%d",value);
+
+           write(fd, buffer, strlen(buffer));
+
+           return *this;
        }
+
+       ~MyOfstream() {
+
+           std::cout << "[CLOSE WRITE FILE]\n";
+
+           close(fd);
+       }
+};
+
+// ===========================================
+// SIMPLE IFSTREAM
+// ============================================
+
+class MyIfstream {
+
+    private:
+       int fd;
+
+    public:
+      MyIfstream(const char* filename) {
+
+          fd = open(filename, O_RDONLY);
+
+          std::cout << "[OPEN FOR READ]\n";
+
+          std::cout << "fd : " << fd << '\n';
+      }   
+
+      void read_all() {
+
+        char buffer[1024];;
+
+        ssize_t bytes = read(fd, buffer, sizeof(buffer) - 1);
+
+        if (bytes > 0) {
+
+            buffer[bytes] = '\0';
+
+            std::cout << "[FILE CONTENT]\n";
+            std::cout << buffer << '\n';
+        }
+      }
+
+      ~MyIfstream() {
+
+        std::cout << "[CLOSE READ FILE]\n";
+
+        close(fd);
+      }
+};
+
+// ===========================================
+// Main
+// ===========================================
+
+int main() {
+    {
+
+    MyOfstream fout("person.txt");
+
+    fout << "Name : TTony Stark\n";
+
+    fout << "Age : ";
+
+    fout << 45;
+
+    fout << "\n";
+}
+{
+    MyIfstream fin("person.txt");
+
+    fin.read_all();
+}
 }
