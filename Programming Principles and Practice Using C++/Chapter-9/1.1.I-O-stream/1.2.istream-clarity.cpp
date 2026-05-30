81cc2b6501b11b47cfbc865d21db1e438af6b454
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-30 08:26:50 +0000

    1.2.istream-clarity.cpp,

---
+++ Programming Principles and Practice Using C++/Chapter-9/1.1.I-O-stream/1.2.istream-clarity.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+
+    std::string input {};
+
+    std::cout << "Enter age: ";
+    std::cin  >> input; // string gather
+
+    std::istringstream ss {input}; 
+
+    int age {};
+    ss >> age; // string to int
+
+    if (!ss) {
+        std::cerr << "Invalid Enter a number.\n";
+        return 1;
+    }
+
+    std::cout << "Your age is: " << age << '\n';
+    return 0;
+}
