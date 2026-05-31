c4562d7b04310702301d32c2f30985ec1286b946
vasanthmalaiyalan <vasanthmalaiyalan321@gmail.com>
2026-05-31 07:17:25 +0000

    1.1.error-handling.cpp,a.out,nmbers.txt,numbers.txt,

--- Programming Principles and Practice Using C++/Chapter-9/1.5.I-O-Error-Handling/1.1.error-handling.cpp
+++ Programming Principles and Practice Using C++/Chapter-9/1.5.I-O-Error-Handling/1.1.error-handling.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 
 std::vector<int> read_numbers(const std::string& file_name) {
 
-    std::ifstream file {file_name};
+    std::ifstream file;
 
     file.exceptions(std::ios::badbit | std::ios::failbit);
+
+    file.open(file_name);
 
     std::vector<int> values {};
 
@@ -21,7 +24,13 @@ std::vector<int> read_numbers(const std::string& file_name) {
 }
 
 int main() {
+
+    std::ofstream create_file("nmbers.txt");
+    create_file << "123 2345 456 12";
+    create_file.close();
+
     try {
+
         const auto values = read_numbers("numbers.txt");
 
         for (int value : values) {
